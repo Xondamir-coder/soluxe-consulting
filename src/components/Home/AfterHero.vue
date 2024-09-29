3ce35d5d32e58ff7d13773217ed7e76acef358eb
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-09-29 10:40:52 +0500

    images resized for various device screens

--- src/components/Home/AfterHero.vue
+++ src/components/Home/AfterHero.vue
@@ -7,8 +7,8 @@
 		<div class="after__images">
 			<img
 				alt="banners"
-				width="2048"
-				height="1366"
+				:width="imageWidth"
+				:height="imageHeight"
 				v-for="(image, i) in images"
 				class="after__image"
 				:src="image"
@@ -58,11 +58,41 @@ import image4 from '@/images/after-4.avif';
 import image5 from '@/images/after-5.avif';
 import image6 from '@/images/after-6.avif';
 import image7 from '@/images/after-7.avif';
+import smallImage1 from '@/images/after-1-small.avif';
+import smallImage2 from '@/images/after-2-small.avif';
+import smallImage3 from '@/images/after-3-small.avif';
+import smallImage4 from '@/images/after-4-small.avif';
+import smallImage5 from '@/images/after-5-small.avif';
+import smallImage6 from '@/images/after-6-small.avif';
+import smallImage7 from '@/images/after-7-small.avif';
+import mediumImage1 from '@/images/after-1-medium.avif';
+import mediumImage2 from '@/images/after-2-medium.avif';
+import mediumImage3 from '@/images/after-3-medium.avif';
+import mediumImage4 from '@/images/after-4-medium.avif';
+import mediumImage5 from '@/images/after-5-medium.avif';
+import mediumImage6 from '@/images/after-6-medium.avif';
+import mediumImage7 from '@/images/after-7-medium.avif';
+
 import { onMounted, ref } from 'vue';
 
 const intervalTime = 2; // seconds
 const isSmall = window.innerWidth < 768;
-const images = [image1, image2, image3, image4, image5, image6, image7];
+const isMedium = window.innerWidth >= 768 && window.innerWidth < 1400;
+const imageWidth = isSmall ? 512 : isMedium ? 1024 : 2048;
+const imageHeight = isSmall ? 288 : isMedium ? 576 : 1152;
+const images = isSmall
+	? [smallImage1, smallImage2, smallImage3, smallImage4, smallImage5, smallImage6, smallImage7]
+	: isMedium
+	? [
+			mediumImage1,
+			mediumImage2,
+			mediumImage3,
+			mediumImage4,
+			mediumImage5,
+			mediumImage6,
+			mediumImage7
+	  ]
+	: [image1, image2, image3, image4, image5, image6, image7];
 const currentImageIndex = ref(0);
 
 onMounted(() => {
