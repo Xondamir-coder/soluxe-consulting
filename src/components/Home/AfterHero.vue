420960c704e40b7cfedf8bf5e5cd8215f713465b
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-09-29 10:51:21 +0500

    preloading images

--- src/components/Home/AfterHero.vue
+++ src/components/Home/AfterHero.vue
@@ -51,33 +51,33 @@
 
 <script setup>
 import SectionLabel from '../SectionLabel.vue';
-import image1 from '@/images/after-1.avif';
-import image2 from '@/images/after-2.avif';
-import image3 from '@/images/after-3.avif';
-import image4 from '@/images/after-4.avif';
-import image5 from '@/images/after-5.avif';
-import image6 from '@/images/after-6.avif';
-import image7 from '@/images/after-7.avif';
-import smallImage1 from '@/images/after-1-small.avif';
-import smallImage2 from '@/images/after-2-small.avif';
-import smallImage3 from '@/images/after-3-small.avif';
-import smallImage4 from '@/images/after-4-small.avif';
-import smallImage5 from '@/images/after-5-small.avif';
-import smallImage6 from '@/images/after-6-small.avif';
-import smallImage7 from '@/images/after-7-small.avif';
-import mediumImage1 from '@/images/after-1-medium.avif';
-import mediumImage2 from '@/images/after-2-medium.avif';
-import mediumImage3 from '@/images/after-3-medium.avif';
-import mediumImage4 from '@/images/after-4-medium.avif';
-import mediumImage5 from '@/images/after-5-medium.avif';
-import mediumImage6 from '@/images/after-6-medium.avif';
-import mediumImage7 from '@/images/after-7-medium.avif';
+import image1 from '/images/after-1.avif';
+import image2 from '/images/after-2.avif';
+import image3 from '/images/after-3.avif';
+import image4 from '/images/after-4.avif';
+import image5 from '/images/after-5.avif';
+import image6 from '/images/after-6.avif';
+import image7 from '/images/after-7.avif';
+import smallImage1 from '/images/after-1-small.avif';
+import smallImage2 from '/images/after-2-small.avif';
+import smallImage3 from '/images/after-3-small.avif';
+import smallImage4 from '/images/after-4-small.avif';
+import smallImage5 from '/images/after-5-small.avif';
+import smallImage6 from '/images/after-6-small.avif';
+import smallImage7 from '/images/after-7-small.avif';
+import mediumImage1 from '/images/after-1-medium.avif';
+import mediumImage2 from '/images/after-2-medium.avif';
+import mediumImage3 from '/images/after-3-medium.avif';
+import mediumImage4 from '/images/after-4-medium.avif';
+import mediumImage5 from '/images/after-5-medium.avif';
+import mediumImage6 from '/images/after-6-medium.avif';
+import mediumImage7 from '/images/after-7-medium.avif';
 
 import { onMounted, ref } from 'vue';
 
 const intervalTime = 2; // seconds
-const isSmall = window.innerWidth < 768;
-const isMedium = window.innerWidth >= 768 && window.innerWidth < 1400;
+const isSmall = window.innerWidth < 600;
+const isMedium = window.innerWidth >= 600 && window.innerWidth < 1280;
 const imageWidth = isSmall ? 512 : isMedium ? 1024 : 2048;
 const imageHeight = isSmall ? 288 : isMedium ? 576 : 1152;
 const images = isSmall
