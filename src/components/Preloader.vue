6276cca7decdf9d051469a7ddbde686d5f70e65e
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-10-04 12:41:25 +0500

    preloader done

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,70 +1,63 @@
 <template>
 	<div :class="{ shown: isPreloaderShown }" class="preloader">
-		<PreloaderLogo ref="logo" />
+		<PreloaderLogo class="preloader__logo" />
 		<div class="preloader__banner"></div>
 	</div>
 </template>
 
 <script setup>
-import { onMounted, ref, useTemplateRef } from 'vue';
+import { onMounted, ref } from 'vue';
 import PreloaderLogo from './PreloaderLogo.vue';
 import gsap from 'gsap';
 
 // Define ref for the child component
-const logoRef = useTemplateRef('logo');
 const isPreloaderShown = ref(true);
 
 onMounted(() => {
-	const paths = Array.from(logoRef.value.logo.querySelectorAll('path'));
-	const pathLengths = paths.map(path => path.getTotalLength());
-	pathLengths.forEach((length, i) => {
-		const fillColor = paths[i].getAttribute('stroke');
-		gsap.timeline({
+	const length = document.querySelector('.preloader__logo .icon').getTotalLength();
+	console.log(length);
+	gsap.timeline()
+		.to('.preloader__logo .icon', {
+			fill: '#C29B69',
+			duration: 2,
+			delay: 1
+		})
+		.from('.preloader__logo', {
+			scale: 2,
+			y: 50
+		})
+		.from(
+			'.preloader__logo .text',
+			{
+				transformOrigin: 'center',
+				scale: 0,
+				opacity: 0
+			},
+			'-=.5'
+		)
+		.from('.preloader__banner', {
+			x: '100%',
+			duration: 1
+		})
+		.to('.preloader', {
+			opacity: 0,
+			display: 'none',
 			onComplete: () => {
 				isPreloaderShown.value = false;
 			}
-		})
-			.to(paths[i], {
-				fill: fillColor,
-				delay: 1.5,
-				duration: 1.5
-			})
-			.to(logoRef.value.logo, {
-				opacity: 0,
-				duration: 0.5,
-				ease: 'none'
-			})
-			.fromTo(
-				'.preloader__banner',
-				{
-					x: '100%'
-				},
-				{
-					x: '-100%',
-					duration: 1
-				},
-				'-=.5'
-			)
-			.to('.preloader', {
-				opacity: 0,
-				display: 'none'
-			});
+		});
 
-		// .to('.preloader', {
-		// 	opacity: 0
-		// });
-		gsap.fromTo(
-			paths[i],
-			{
-				strokeDashoffset: length,
-				strokeDasharray: length
-			},
-			{
-				strokeDashoffset: 0,
-				duration: 25
-			}
-		);
-	});
+	gsap.fromTo(
+		'.preloader__logo .icon',
+		{
+			strokeDasharray: length,
+			strokeDashoffset: length
+		},
+		{
+			strokeDashoffset: 0,
+			duration: 40
+		}
+	);
 });
 </script>
 
@@ -73,14 +66,14 @@ onMounted(() => {
 	overflow: hidden;
 	position: fixed;
 	inset: 0;
-	background-color: #fff;
+	background-color: #013d45;
 	z-index: 101;
 	display: grid;
 	place-content: center;
 	&__banner {
 		position: absolute;
 		inset: 0;
-		background-color: var(--yellow);
+		background-color: #fff;
 	}
 }
 </style>
